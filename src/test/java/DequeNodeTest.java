6280871fbac1d330c4f7652bea226da6dbda633f
Jose-Tapia-Catena <jmtapiacatena@uma.es>
2022-03-17 12:03:43 +0100

    Actualizo proyecto, añado plugins

--- src/test/java/DequeNodeTest.java
+++ src/test/java/DequeNodeTest.java
@@ -1,7 +1,15 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class DequeNodeTest {
 
+    private DequeNode dequeNode;
 
+    @Test
+    public void shouldComputeRaisAnExceptionIfTheItemIsNull(){
+        assertThrows(RuntimeException.class, () -> new DequeNode<>(null, null, null) );
+    }
 
 }
