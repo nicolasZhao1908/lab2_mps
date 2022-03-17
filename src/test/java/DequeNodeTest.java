330009a5438d45b3a7d94a91c8cc9526f09ae9a2
nicoZhao1908 <0619971117@uma.es>
2022-03-17 12:19:54 +0100

    Update DequeNodeTest (Insert 2 tests)

--- src/test/java/DequeNodeTest.java
+++ src/test/java/DequeNodeTest.java
@@ -1,13 +1,40 @@
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class DequeNodeTest {
+class DequeNodeTest<T> {
     private DequeNode dequeNode;
-
+    
     @AfterEach
     public void finish(){
         dequeNode = null;
+    }
+
+    @Test
+    public void shouldHaveUpdatedNextIfSetNewNext(){
+        Integer item = 0;
+        dequeNode = new DequeNode<>(item,null,null);
+        DequeNode<T> oldValue;
+        oldValue = dequeNode.getNext();
+        DequeNode<T> newValue;
+        newValue = new DequeNode(item,null,null);
+        dequeNode.setNext(newValue);
+
+        assertNotEquals(oldValue,newValue);
+    }
+
+    @Test
+    public void shouldHaveUpdatedNextIfSetNewPrevious(){
+        Integer item = 0;
+        dequeNode = new DequeNode<>(item,null,null);
+        DequeNode<T> oldValue;
+        oldValue = dequeNode.getPrevious();
+        DequeNode<T> newValue;
+        newValue = new DequeNode(item,null,null);
+        dequeNode.setPrevious(newValue);
+
+        assertNotEquals(oldValue,newValue);
     }
 }
