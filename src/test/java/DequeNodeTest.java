18661d64f087168a938b28176a33ddd2be0346f3
VictorMadrona <victormadrona@uma.es>
2022-03-17 12:22:57 +0100

    testNewItemSettedIsUpdated completed

--- src/test/java/DequeNodeTest.java
+++ src/test/java/DequeNodeTest.java
@@ -1,5 +1,34 @@
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
-class DequeNodeTest {
+class DequeNodeTest{
+
+    @Test
+    public void testNewItemSettedIsNotNull(){
+
+        DequeNode<Integer> d = new DequeNode<>(null,null,null);
+
+        Integer newItem = null;
+    }
+
+    @Test
+    public void testNewItemSettedIsUpdated(){
+        Integer newItem = 1;
+
+        DequeNode<Integer> d = new DequeNode<>(null,null,null);
+        d.setItem(newItem);
+        assertTrue(newItem.equals(d.getItem()));
+
+        d = new DequeNode<>(1,null,null);
+        d.setItem(newItem);
+        assertTrue(newItem.equals(d.getItem()));
+
+        d = new DequeNode<>(2,null,null);
+        d.setItem(newItem);
+        assertTrue(newItem.equals(d.getItem()));
+    }
 
 }
