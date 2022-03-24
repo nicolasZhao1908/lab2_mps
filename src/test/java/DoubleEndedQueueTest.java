3d27f934878c9332cba47362f3fd330f093132ed
VictorMadrona <victormadrona@uma.es>
2022-03-24 11:53:26 +0100

    Two new Test in DoubleEndedQueueTest from method delete

--- src/test/java/DoubleEndedQueueTest.java
+++ src/test/java/DoubleEndedQueueTest.java
@@ -187,4 +187,24 @@ class DoubleEndedQueueTest {
         assertThrows(RuntimeException.class,() -> doubleEndedQueueList.deleteLast());
     }
 
+    @Test
+    public void shouldRaiseExceptionWhenDeletingAnEmptyNode(){
+        assertThrows(RuntimeException.class, () -> doubleEndedQueueList.delete(null));
+    }
+
+    @Test
+    public void shouldRaiseExceptionWhenDeletingANodeNotInTheList(){
+        DoubleEndedQueueList<Integer> list = new DoubleEndedQueueList<>();
+        DequeNode node1 = new DequeNode(1,null,null);
+        DequeNode node2 = new DequeNode(2,null, null);
+        DequeNode node3 = new DequeNode(3,null, null);
+        DequeNode node4 = new DequeNode(4,null,null);
+
+        list.append(node1);
+        list.append(node2);
+        list.append(node3);
+
+        assertThrows(RuntimeException.class, () -> doubleEndedQueueList.delete(node4));
+    }
+
 }
