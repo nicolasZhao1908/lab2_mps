ffb5ecb0e2af510c808e213e9f0be58f594edbff
VictorMadrona <victormadrona@uma.es>
2022-03-24 12:17:48 +0100

    Equals and hashCode in DequeNode

--- src/main/java/DequeNode.java
+++ src/main/java/DequeNode.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 /**
  * Class representing a node of a double-ended queue (deque). Each node has pointers to the next and
  * previous nodes. The previous and next of the first and last node of the deque is null.
@@ -52,6 +54,19 @@ public class DequeNode<T> {
 
     public boolean isNotATerminalNode() {
         return (!isFirstNode() && !isLastNode());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        DequeNode<?> dequeNode = (DequeNode<?>) o;
+        return Objects.equals(item, dequeNode.item) && Objects.equals(next, dequeNode.next) && Objects.equals(previous, dequeNode.previous);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(item, next, previous);
     }
 
 
